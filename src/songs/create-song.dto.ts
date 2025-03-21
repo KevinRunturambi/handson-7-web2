0b768902b3d9421915e5163350c13a088bbf1372
Kevin Runturambi <2381034@unai.edu>
2025-03-21 10:28:50 +0700

    Commit

--- src/songs/create-song.dto.ts
+++ src/songs/create-song.dto.ts
@@ -1,4 +1,9 @@
+import { IsString, isString } from "class-validator";
+
 export class CreateSongDTO{
+    @IsString()
     title: string;
+
+    @IsString()
     artist: string;
 }
--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,34 +1,35 @@
-import {Body,Controller,Delete,Get,Param,Post,Put,} 
-from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseInterceptors, ValidationPipe } from '@nestjs/common';
 import { SongsService } from './songs.service';
 import { CreateSongDTO } from './create-song.dto';
+import { ExecutionTime } from 'src/ExecutionTime.interceptor';
 
 @Controller('songs')
 export class SongsController {
-  constructor(private songService: SongsService) {}
+    constructor(private songService: SongsService){}
+    
+    @Post()
+    @UseInterceptors(ExecutionTime)
+    create(@Body( new ValidationPipe) createSongDTO: CreateSongDTO){
+        return this.songService.create(createSongDTO);
+    }
 
-  @Post()
-  create(@Body() createSongDTO: CreateSongDTO) {
-    return this.songService.create(createSongDTO);
-  }
+    @Get()
+    findAll(){
+        return this.songService.findAll();
+    }
 
-  @Get()
-  findAll() {
-    return this.songService.findAll();
-  }
+    @Get(':id')
+    findOne(@Param('id') id: number){
+        return this.songService.findOne(id);
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: number) {
-    return this.songService.findOne(id);
-  }
+    @Put(':id')
+    update(@Param('id')id: number, @Body() createSongDTO: CreateSongDTO){
+        return this.songService.updateOne(id, createSongDTO);
+    }
 
-  @Put(':id')
-  update(@Param('id') id: number, @Body() createSongDTO: CreateSongDTO) {
-    return this.songService.updateOne(id, createSongDTO);
-  }
-
-  @Delete(':id')
-  delete(@Param('id') id: number) {
-    return this.songService.delete(id);
-  }
+    @Delete(':id')
+    delete(@Param('id') id: number){
+        return this.songService.delete(id);
+    }
 }
